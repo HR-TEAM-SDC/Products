b2741b95b027de9250d6090e7fd90e9b73e760f0
Thanh Ha <thanh3203@gmail.com>
2022-01-14 20:33:35 -0800

    deployed options

--- db/connect.js
+++ db/connect.js
@@ -1,24 +1,24 @@
 const { Pool } = require('pg');
 
-// const pool = new Pool({
-//   host: '54.67.112.18',
-//   port: 5432,
-//   user: 'postgres',
-//   password: 'password',
-//   database: 'products',
-//   max: 20,
-//   idleTimeoutMillis: 30000,
-//   connectionTimeoutMillis: 2000
-// });
-
 const pool = new Pool({
   host: 'localhost',
   port: 5432,
-  user: 'thha3203',
+  user: 'postgres',
+  password: 'password',
   database: 'products',
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000
 });
+
+// const pool = new Pool({
+//   host: 'localhost',
+//   port: 5432,
+//   user: 'thha3203',
+//   database: 'products',
+//   max: 20,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 2000
+// });
 
 module.exports = pool;
